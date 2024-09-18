bbe1bca1eced1cadcadf34d1bfe62f086536182c
Brian Sarmiento <briansarmiento.93@gmail.com>
2024-09-18 20:06:31 -0300

    C

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let v = "hecho en javascript";
+document.write("Esta es una variable: "+v);
+console.log(v);
+
+const s=0;
+
+// Comentario de una linea
+/*
+    Comentario
+    de
+    varias
+    lineas
+*/
+
