8681f5c5abe85365b751b8a5ad81337b14b660e0
Brian Sarmiento <briansarmiento.93@gmail.com>
2024-10-17 13:24:20 -0300

    Agrego clase producto con productos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 
+class Producto {
+    constructor(codigo, nombre, precio, stock, img ) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.img = img;
+    }
+}
+
+const prod1 = new Producto(1,"Flores",4000.00,10,"../assets/img/imagenes/flores.jpg")
+const prod2 = new Producto(2,"Bolso de Cristal + Set Matero",15000.00,5,"../assets/img/imagenes/bolso_cristal_mas_set_matero.jpg")
+const prod3 = new Producto(3,"Almohadón Matero",22000.00,7,"../assets/img/imagenes/almohadon_matero.jpg")
+const prod4 = new Producto(4,"Bombones aromáticos x 15",10500.00,50,"../assets/img/imagenes/bombones_aromaticos.jpg")
+const prod5 = new Producto(5,"Yerbero y azucarero",5500,12,"../assets/img/imagenes/yerbero_azucarero.jpg")
+const prod6 = new Producto(6,"Souvenires de velas x 100",20000.00,2,"../assets/img/imagenes/souvenir_velas_x100.jpg")
+/*
+let main = document.getElementById("princ")
+let img = document.createElement("div")
+img.innerHTML =
+`<img src="${prod1.img}" alt="${prod1.nombre}">
+<img src="${prod2.img}" alt="${prod2.nombre}">
+<img src="${prod3.img}" alt="${prod3.nombre}">
+<img src="${prod4.img}" alt="${prod4.nombre}">
+<img src="${prod5.img}" alt="${prod5.nombre}">
+<img src="${prod6.img}" alt="${prod6.nombre}">`
+main.appendChild(img)
+*/
+/*
+let img = document.createElement("div")
+img.innerHTML = `
+<img src="${prod1.img}" alt="${prod1.nombre}">`
+document.body.appendChild(img);
+*/
+/*
 let opcion = 0
 let total = 0
 let carrito = ''
@@ -129,4 +164,4 @@ console.log("¡Bienvenido a Margarita Regalos!")
 let [prodElegidos, totalProd] = (agregarProductos())
 confirmCompra(prodElegidos, totalProd)
 console.log('¡Gracias por elegirnos!')
-
+*/
