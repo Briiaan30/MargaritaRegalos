cea811f53c998fdb62d10307d5f4bc0437d69772
Brian Sarmiento <briansarmiento.93@gmail.com>
2024-11-02 15:58:35 -0300

    Js2 en desarrollo

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,88 @@
-let cards = document.createElement('div');
-let cardMain = document.querySelector('.cards-item')
-cards.innerHTML = '<h1>hola</h1>';
+let productos = [
+    {
+        codigo: 1,
+        nombre: "Flores",
+        descr: "Flores hermosas y hechas con amor",
+        precio: 4000.00,
+        stock: 10,
+        img: "../assets/img/imagenes/flores.jpg"
+    },
+    {
+        codigo: 2,
+        nombre: "Bolso de Cristal + Set Matero",
+        descr: "Perfecto para el conjunto de mate",
+        precio: 15000.00,
+        stock: 5,
+        img: "../assets/img/imagenes/bolso_cristal_mas_set_matero.jpg"
+    },
+    {
+        codigo: 3,
+        nombre: "Almohadón Matero",
+        descr: "Ideal para ir al rio y usarlo sobre el cesped o la arena",
+        precio: 22000.00,
+        stock: 7,
+        img: "../assets/img/imagenes/almohadon_matero.jpg"
+    },
+    {
+        codigo: 4,
+        nombre: "Bombones Aromáticos x 15",
+        descr: "Bombones para darle un nuevo aroma a tu vida",
+        precio: 10500.00,
+        stock: 50,
+        img: "../assets/img/imagenes/bombones_aromaticos.jpg"
+    },
+    {
+        codigo: 5,
+        nombre: "Yerbero y Azucarero",
+        descr: "Conjunto personalizable para la yerba y el azucar",
+        precio: 5500,
+        stock: 12,
+        img: "../assets/img/imagenes/yerbero_azucarero.jpg"
+    },
+    {
+        codigo: 6,
+        nombre: "Souvenires de Velas x 100",
+        descr: "Excelente regalo para un casamiento o una fiesta de 15 de tu hija",
+        precio: 20000.00,
+        stock: 0,
+        img: "../assets/img/imagenes/souvenir_velas_x100.jpg"
+    },
+]
+
+let carrito = []
+
+/*
+ let idItem = document.getElementById('idItemPrecio')
+ console.log(idItem)
+ console.log(idItem.innerHTML)
+ console.log(idItem.innerText)
+
+ idItem.innerText = "holas"
+ console.log(idItem.innerText)
+*/
+
+
+let sectionCards = document.getElementById('sectionCards')
+console.log(sectionCards)
+
+productos.forEach(item => {
+    console.log(item.nombre)
+    sectionCards.innerHTML += `<div class="pt-2 pb-2">
+    <div class="container cards-container">
+        <div class="cards-item">
+            <div class="card-descripcion">
+                <h2>${item.nombre}</h2>
+                <p>${item.descr}</p>
+            </div>
+            <img src=${item.img}>
+            <div class="boton-container d-flex">
+                <button class="boton-agregar">Agregar</button>
+                <div class="boton-precio d-flex">
+                    <p><b>$${item.precio}</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`
+    
+});
